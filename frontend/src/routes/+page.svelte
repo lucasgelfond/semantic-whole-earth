331794fd828347928c933e2a379dd6706a99c9a2
Lucas Gelfond <lucas_gelfond@brown.edu>
2025-03-10 18:12:03 -0700

    working pglite

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { getDB, initSchema, countRows, seedDb } from '../utils/db';
+  import { onMount } from 'svelte';
+
+  let content = [];
+  let initializing = false;
+  let db;
+
+  onMount(async () => {
+    const setup = async () => {
+      initializing = true;
+      db = await getDB();
+      await initSchema(db);
+      let count = await countRows(db, 'embeddings');
+
+      if (count === 0) {
+        seedDb(db);
+      }
+
+      // Get Items
+      const items = await db.query('SELECT content FROM embeddings order by content asc');
+      content = items.rows.map((x) => x.content);
+    }
+
+    if (!db && !initializing) {
+      setup();
+    }
+  });
+</script>
+
+<pre>{JSON.stringify(content)}</pre>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,27 @@
+import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [sveltekit(), tailwindcss()],
+	optimizeDeps: {
+		exclude: ['@electric-sql/pglite']
+	},
+	server: {
+		fs: {
+			// Allow serving files from node_modules
+			allow: ['node_modules/@electric-sql/pglite/dist/']
+		}
+	},
+	build: {
+		target: 'esnext',
+		rollupOptions: {
+			// Make sure these assets are included in the build
+			external: [
+				'/node_modules/.vite/deps/postgres.wasm',
+				'/node_modules/.vite/deps/postgres.data',
+				'/node_modules/.vite/vector.tar.gz'
+			]
+		}
+	}
 });
